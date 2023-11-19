61232fce1cdf788c2c328bd45da7f6baeee8b3d3
AvadhutK01 <143504930+AvadhutK01@users.noreply.github.com>
2023-11-20 00:15:52 +0530

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -4,10 +4,7 @@ const bodyParser = require('body-parser');
 const adminRoute = require('./Routes/adminRoute');
 const shopRoute = require('./Routes/shopRoute');
 const mongoConnect = require('./util/database');
-<<<<<<< HEAD:BackEnd/app.js
 require('dotenv').config();
-=======
->>>>>>> f40e11928b14c010845799845653d776305aba26:app.js
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'Public')));
