72509fcff7d4fa6300cb0292fed9a2888bf98e43
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-11-19 23:37:04 +0530

    Initial commit and setup

---
+++ Routes/shopRoute.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const shopRoute = express.Router();
+module.exports = shopRoute;
---
+++ app.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoute = require('./Routes/adminRoute');
+const shopRoute = require('./Routes/shopRoute');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'Public')));
+app.use('/admin', adminRoute);
+app.use(shopRoute);
+app.listen(3000, () => console.log("Server is running"));
